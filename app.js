95d4ee483b681eb7406f71645921c8688e5999a0
andreucc <andreucc@protonmail.com>
2019-02-28 00:51:03 +0100

    index done

--- app.js
+++ app.js
@@ -17,11 +17,32 @@ app.get('/', (req, res, next) => {
 app.get('/beers', (req, res, next) => {
   punkAPI.getBeers()
     .then(beers => {
-      res.render('beers', { beers });
+      beers = beers;
+      res.render('beers');
     })
     .catch(error => {
       console.log(error);
     });
+});
+
+// -- 404 and error handler
+
+// NOTE: requires a views/not-found.ejs template
+app.use((req, res, next) => {
+  res.status(404);
+  res.render('not-found');
+});
+
+// NOTE: requires a views/error.ejs template
+app.use((err, req, res, next) => {
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
+
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500);
+    res.render('error');
+  }
 });
 
 app.listen(3000);
